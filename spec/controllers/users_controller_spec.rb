2aa46bb559b2995694e7ce3ca67a34fd3bb87b72
Bob Showalter <showaltb@gmail.com>
2011-06-29 21:17:52 -0400

    Finished layout and routes

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  render_views
+
+  describe "GET 'new'" do
+    before(:each) do
+      get 'new'
+    end
+    it "should be successful" do
+      response.should be_success
+    end
+    it "should have the right title" do
+      response.should have_selector("title",
+        :content => "#{@base_title} | Sign up")
+    end
+  end
+
+end
